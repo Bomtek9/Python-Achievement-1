673201ec13194d62eff5751df587770dd0e85dbc
Bomtek9 <132293701+Bomtek9@users.noreply.github.com>
2024-01-17 22:28:27 -0600

    add 2 numbers

---
+++ venv/add.py
@@ -0,0 +1,11 @@
+# Get user input for the first number
+a = int(input("Enter first number: "))
+
+# Get user input for the second number
+b = int(input("Enter second number: "))
+
+# Add the two numbers and store the result in variable c
+c = a + b
+
+# Print the result
+print(c)
