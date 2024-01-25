04ad70764e10e527248b06b9390bae433f4d8f05
Bomtek9 <132293701+Bomtek9@users.noreply.github.com>
2024-01-25 10:14:35 -0600

    Task requirements

---
+++ Exercise-1.3/name_capitalizer.py
@@ -0,0 +1,11 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+last_name=last_name.capitalize()
+first_name=first_name.capitalize()
+
+print("Your name is", first_name, last_name)
+
+a=int(input("Enter a number: "))
+b=int(input("Enter a number to add it to: "))
+
+print("The sume of these numbers is " + str(a + b))
---
+++ Exercise-1.3/number_adding.py
@@ -0,0 +1,4 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter a number to add it to: "))
+
+print("The sume of these numbers is " + str(a + b))
