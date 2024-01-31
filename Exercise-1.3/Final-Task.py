8f2975582ad3f80d399342c25275cbf2888f65e5
Bomtek9 <132293701+Bomtek9@users.noreply.github.com>
2024-01-31 10:14:15 -0600

    For Loop
    for loop depending on number of recipes the user wants to enter (n)

--- Exercise-1.3/Final-Task.py
+++ Exercise-1.3/Final-Task.py
@@ -11,12 +11,26 @@ def take_recipe():
         recipe = {'name': name, 'cooking_time': cooking_time, 'ingredients': ingredients}
         return recipe
 
-recipe_data = take_recipe()
 
-# Access the recipe information using the dictionary keys
-print("Recipe Name:", recipe_data['name'])
-print("Cooking Time:", recipe_data['cooking_time'])
-print("Ingredients:", recipe_data['ingredients'])
 
 n = input(int("How many more recipes do you want to add? "))
+
+# Run a for loop to take n recipes
+for _ in range(n):
+
+    # Run take_recipe() and store its return output in a variable called recipe
+    recipe_data = take_recipe()
+
+    # Access the recipe information using the dictionary keys
+    print("Recipe Name:", recipe_data['name'])
+    print("Cooking Time:", recipe_data['cooking_time'])
+    print("Ingredients:", recipe_data['ingredients'])
+
+    # Run another loop to check and add new ingredients to ingredients_list
+    for ingredient in recipe_data['ingredients']:
+        if ingredient not in ingredients_list:
+            ingredients_list.append(ingredient)
+
+# Print the final ingredients list
+print("Ingredients List:", ingredients_list)
 
